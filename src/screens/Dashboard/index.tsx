8c197dd46bd02d10b989d2c785e0b3673e5b877c
David Carmo <davidsc2812@gmail.com>
2022-01-16 03:14:40 -0300

    recriacao do app

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Container, Title } from "./styles";
+export function Dashboard() {
+  return (
+    <Container>
+      <Title>Dashboard</Title>
+    </Container>
+  );
+}

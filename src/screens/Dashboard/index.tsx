4b643719da0ba0ca4c6ac9e0619f64991f87e2ef
David Carmo <davidsc2812@gmail.com>
2022-01-17 23:46:47 -0300

    criacao do header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,9 +1,35 @@
 import React from "react";
-import { Container, Title } from "./styles";
+
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+} from "./styles";
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/43228973?s=96&v=4",
+              }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>David</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
